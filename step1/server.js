6976bb2f1f22677429df18fb31ef338c4bde0567
Jemmy <jemmy_lin@wistron.com>
2020-07-31 14:01:45 +0800

    [Add] add router

--- step1/server.js
+++ step1/server.js
@@ -1,18 +1,41 @@
 const Koa = require('koa');
 const Logger = require('koa-logger');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new Router();
 
-//response
-app.use(Logger());
-app.use((ctx,next) => {
-    const start_time = Date.now();
-    return next().then(() => {
-        const ms = Date.now() - start_time;
-        console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-    });
+// Router -> /
+router.get('/', async(ctx) => {
+    ctx.body = 'Hello Koa2 body';
+});
+// Router -> /ready
+router.get('/ready', async(ctx) => {
+    ctx.body = 'Ready Content';
+});
+// Router -> /admin
+router.get('/admin', async(ctx) => {
+    let name = ctx.query.name;
+    let talk = ctx.query.talk;
+    ctx.body =`<p>${name} : ${talk}</p>`;
+});
+// Login form
+router.get('/login', async(ctx) => {
+    ctx.body = `
+        <form method="POST" action="/login">
+            <label>UserName</label>
+            <input name="user" /><br/>
+            <button type="submit">submit</button>
+        </form>
+    `;
+});
+// Send form
+router.post('/login', async(ctx) => {
+    let user = ctx.request.body.user;
+    ctx.body = `<p>Welcome, ${user}!</p>`;
 });
 
+app.use(router.routes());
+app.use(bodyParser());
 app.listen(3000);
